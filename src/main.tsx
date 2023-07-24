cb34a59777a2fb6aed8bdb0aa0a35fa9b140a3ee
Phumrapee Limpianchop <git@rayriffy.com>
2023-07-24 23:32:55 +0900

    feat: init

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+// we work outside StrictMode for now, i dont want to debug double events call rn
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <App />
+)
