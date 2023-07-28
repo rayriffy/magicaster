24501db158e5133f111338f5f7298ac23fe1fe25
Tauhoo <tauhoo_ice@hotmail.com>
2023-07-28 18:13:16 +0700

    set up graphic asset system

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,30 @@ import { useEffect, useState } from 'react'
 import GameGUI from './components/GameGUI'
 import './App.css'
 import { GameState } from './logic.ts'
+import * as graphicAssets from './graphic/assets/index.ts'
+import * as PIXI from 'pixi.js'
 
 function App() {
   const [game, setGame] = useState<GameState>()
+  const [loadingAssets, setLoadingAssets] = useState<boolean>(true)
+
   const [characterSelections, setCharacterSelections] = useState<{
     [playerID: string]: number
   }>({ test: 0 })
+
   useEffect(() => {
     Rune.initClient({
       onChange: ({ newGame }) => {
         setGame(newGame)
       },
     })
+    graphicAssets.load().then(() => {
+      setLoadingAssets(false)
+      console.log(PIXI.Assets.get('wizard'))
+    })
   }, [])
 
-  if (!game) {
+  if (!game && !loadingAssets) {
     return <div>Loading...</div>
   }
 
---
+++ src/graphic/assets/index.ts
@@ -0,0 +1,30 @@
+import * as PIXI from 'pixi.js'
+import { bundle as wizardBundle, getSpritesheet } from './wizard'
+
+type Assets = {
+  wizard: { spritesheet: PIXI.Spritesheet }
+}
+
+let assets: Assets | null = null
+
+export const load = async () => {
+  let bundles: PIXI.ResolverBundle[] = [wizardBundle]
+
+  await PIXI.Assets.init({
+    manifest: {
+      bundles: bundles,
+    },
+  })
+
+  for (const bundle of bundles) {
+    await PIXI.Assets.loadBundle(bundle.name)
+  }
+
+  const wizardSpritesheet = await getSpritesheet()
+
+  assets = {
+    wizard: { spritesheet: wizardSpritesheet },
+  }
+}
+
+export default assets
