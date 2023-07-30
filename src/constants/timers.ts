4abb3d36add3be41244d682bc0b28fd029cb8cb2
Phumrapee Limpianchop <git@rayriffy.com>
2023-07-31 00:08:01 +0900

    fix: time

--- src/constants/timers.ts
+++ src/constants/timers.ts
@@ -1,7 +1,7 @@
 import { GameState } from '../logic'
 
 export const getPhaseEndTime = (phase: GameState['phase']) => {
-  if (phase === 'build_word') return dayjs().add(15, 'seconds')
+  if (phase === 'build_word') return Rune.gameTimeInSeconds() + 15
   // else if (phase === 'show_score') return dayjs().add(5, 'seconds')
   if (phase === 'planning') return Rune.gameTimeInSeconds() + 10 * 60 * 60
   // else if (phase === 'activation') return dayjs().add(30, 'seconds')
